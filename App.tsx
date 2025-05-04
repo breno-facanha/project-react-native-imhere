1a66f6e400f41fbd28dd0a3c6b916d8ccc28d67b
Breno Façanha <brenofacanha.ti@gmail.com>
2025-05-04 16:55:35 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity  } from 'react-native';
+import { Fragment } from 'react'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.eventname}> 
+        Nome do evento
+      </Text>
+      <Text style={styles.eventDate}>
+        Domingo, 04 de Maio de 2025
+      </Text>
+
+      <TouchableOpacity style={styles.botao}>
+        <Text style={styles.textBotao}>Olá mundo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.botao}>
+        <Text style={styles.textBotao}>Olá mundo</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#131016',
+    padding: 24
   },
+  eventname:{
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16
+  },
+  botao: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 10
+  },
+  textBotao:{
+    color: 'white', 
+    fontWeight: 'bold'
+  }
 });
